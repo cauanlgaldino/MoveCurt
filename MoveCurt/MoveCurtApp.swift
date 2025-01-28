2c53308a94033cfe776780f330ed97f71b95eb22
cauanlgaldino <cauanlgaldino@gmail.com>
2025-01-28 17:21:32 -0300

    Initial Commit

---
+++ MoveCurt/MoveCurtApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveCurtApp.swift
+//  MoveCurt
+//
+//  Created by Cauan Lopes Galdino on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MoveCurtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
